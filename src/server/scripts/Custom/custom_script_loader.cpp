d15dbcedcd36bd21efd9e352a181b6fda8845eac
zeros <carloscastilla31@gmail.com>
2017-07-18 10:11:36 -0500

    update

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -21,69 +21,35 @@
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 
-
-//npc Apuesta
-
-//NPC reforjador
-
-//GuildHouse NPC
-
- //trangmo
-
-//Visual enchat
-
- //Monturas por cuenta
-void AddSC_accontmounts();
- //Boss announcer
-void AddSC_Boss_Announcer();
-//juego
-
-//reset instance
-
-//top arena
-
-//PVP Syetm
-//chat Censor
+void AddSC_arena_spectator_script();
 void AddSC_System_Censure();
-// world CHat
 void AddSC_World_Chat();
-// Promo
+void AddSC_accontmounts();
+void AddSC_npc_blood_money();
+void AddSC_Boss_Announcer();
+void AddSC_item_enchant_visuals();
+void AddSC_guildmaster();
+void AddSC_npc_arena_setup();
+void AddSC_npc_tic_tac_toe();
+void AddSC_Creature_Script();
 void AddSC_npc_promo();
-void AddSC_cs_promo();
-//Arena Espectador
-
+void AddSC_REFORGER_NPC();
+void AddSC_Transmogrification();
 
 void AddCustomScripts()
 {
-	//npc Apuesta
-	
-	 //NPC reforjador
-	
-	 //GuildHouse NPC
-	
-	  //trangmo
-     
-	 //Visual enchat
-    
-	  //Monturas por cuenta
-     AddSC_accontmounts();
-	  //Boss announcer
-     AddSC_Boss_Announcer();
-	 //juego
-   
-	//reset instance
-
-	 //top arena
-  
-	 //PVP Syetm
-	 //chat Censor
- 	 AddSC_System_Censure();
-	 // world CHat
-     AddSC_World_Chat();
-	 // Promo
-	 AddSC_npc_promo();
-	 AddSC_cs_promo();
-	//Arena Espectador
-     
-	 
+	AddSC_arena_spectator_script();
+	AddSC_System_Censure();
+	AddSC_World_Chat();
+	AddSC_accontmounts();
+	AddSC_npc_blood_money();
+	AddSC_Boss_Announcer();
+    AddSC_item_enchant_visuals();
+	AddSC_guildmaster();
+	AddSC_npc_arena_setup();
+	AddSC_npc_tic_tac_toe();
+	AddSC_Creature_Script();
+	AddSC_npc_promo();
+	AddSC_REFORGER_NPC();
+   AddSC_Transmogrification();
 }
